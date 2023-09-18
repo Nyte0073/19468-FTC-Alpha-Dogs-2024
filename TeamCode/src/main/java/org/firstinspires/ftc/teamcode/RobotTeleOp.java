c104b450fb8b75e01e90c7a9dec1f3c6d41187f8
Luca Iacovelli <liaco1306@gmail.com>
2023-09-18 13:35:31 -0400

    create subsystem foundation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleOp.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.subsystems.Mecanum;
 
@@ -10,13 +13,23 @@ public class RobotTeleOp extends LinearOpMode {
 
     Mecanum s_Drivetrain;
 
+    private ElapsedTime runtime = new ElapsedTime();
+
     @Override
     public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-        s_Drivetrain = new Mecanum();
+        s_Drivetrain = new Mecanum(hardwareMap);
+
+        waitForStart();
+        runtime.reset();
 
         while(opModeIsActive()) {
-            s_Drivetrain.teleop();
+            s_Drivetrain.teleop(gamepad1);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
         }
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Mecanum.java
@@ -1,26 +1,78 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Mecanum {
 
     DcMotor frontLeft0, frontRight1, backLeft2, backRight3;
 
-    public void Mecanum() {
+    public Mecanum(HardwareMap hardwareMap) {
 
-        frontLeft0 = HardwareMap.get(DcMotor.class, "frontLeft0");
-        frontLeft0 = HardwareMap.get(DcMotor.class, "frontRight1");
-        frontLeft0 = HardwareMap.get(DcMotor.class, "backLeft2");
-        frontLeft0 = HardwareMap.get(DcMotor.class, "backRight3");
+        frontLeft0 = hardwareMap.get(DcMotor.class, "frontLeft0");
+        frontRight1 = hardwareMap.get(DcMotor.class, "frontRight1");
+        backLeft2 = hardwareMap.get(DcMotor.class, "backLeft2");
+        backRight3 = hardwareMap.get(DcMotor.class, "backRight3");
 
         frontLeft0 = motorConfig(frontLeft0);
         frontRight1 = motorConfig(frontRight1);
         backLeft2 = motorConfig(backLeft2);
         backRight3 = motorConfig(backRight3);
 
+        frontLeft0.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight1.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft2.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight3.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void teleop(Gamepad gamepad1) {
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x; // * 1.1
+        double rx = gamepad1.right_stick_x;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        double frontLeftPower = (y + x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        double[] power = {frontLeftPower, frontRightPower, backLeftPower, backRightPower};
+
+        setPower(power);
+
+    }
+
+    public void setPower(double[] power) {
+        frontLeft0.setPower(power[0]);
+        frontRight1.setPower(power[1]);
+        backLeft2.setPower(power[2]);
+        backRight3.setPower(power[3]);
+    }
+
+    public double[] getPower() {
+        double[] power = new double[4];
+
+        power[0] = frontLeft0.getPower();
+        power[1] = frontRight1.getPower();
+        power[2] = backLeft2.getPower();
+        power[3] = backRight3.getPower();
+
+        return power;
+    }
+
+    public void periodic(Telemetry telemetry) {
+        telemetry.addLine("Drive Motors");
+        telemetry.addLine("Front Left: " + getPower()[0]);
+        telemetry.addLine("Front Right: " + getPower()[1]);
+        telemetry.addLine("Back Left: " + getPower()[2]);
+        telemetry.addLine("Back Right: " + getPower()[3]);
     }
 
     /**
