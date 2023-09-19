7615db626fd0a849580dd3e9931bd5a45351c2f9
Luca Iacovelli <liaco1306@gmail.com>
2023-09-19 13:47:04 -0400

    drivetrain code broken :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Mecanum.java
@@ -32,20 +32,23 @@ public class Mecanum {
     }
 
     public void teleop(Gamepad gamepad1) {
-        double y = -gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x; // * 1.1
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
         double rx = gamepad1.right_stick_x;
 
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-
         double frontLeftPower = (y + x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
         double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
         double backRightPower = (y + x - rx) / denominator;
 
-        double[] power = {frontLeftPower, frontRightPower, backLeftPower, backRightPower};
-
-        setPower(power);
+        frontLeft0.setPower(frontLeftPower);
+        backLeft2.setPower(backLeftPower);
+        frontRight1.setPower(frontRightPower);
+        backRight3.setPower(backRightPower);
 
     }
 
