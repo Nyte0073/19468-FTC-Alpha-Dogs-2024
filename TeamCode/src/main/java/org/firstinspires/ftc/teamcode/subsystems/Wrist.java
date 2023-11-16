849791b023bdeea21b578f4b428dbde9325b15b5
Luca Iacovelli <liaco1306@gmail.com>
2023-11-16 13:50:57 -0500

    remove left write, invert

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Wrist.java
@@ -10,50 +10,40 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Wrist {
 
-    Servo rightWrist0, leftWrist2;
+    Servo rightWrist0;
 
-    double teleopAngle = 0;
+    double teleopAngle = WristConstants.homeAngle;
 
     public Wrist(HardwareMap hardwareMap) {
         rightWrist0 = hardwareMap.get(Servo.class, WristConstants.rightWristServo);
-        leftWrist2 = hardwareMap.get(Servo.class, WristConstants.leftWristServo);
 
         rightWrist0.setDirection(WristConstants.rightInvert);
-        leftWrist2.setDirection(WristConstants.leftInvert);
     }
 
     public double getAngle() {
-        return ((rightWrist0.getPosition() * 300) + (leftWrist2.getPosition() * 300)) / 2;
-    }
-
-    public double getRightAngle() {
-        return rightWrist0.getPosition() * 300;
-    }
-
-    public double getLeftAngle() {
-        return leftWrist2.getPosition() * 300;
+        return (rightWrist0.getPosition() * 300);
     }
 
     public void setAngle(double angle) {
         rightWrist0.setPosition((angle / 300));
-        leftWrist2.setPosition((angle / 300));
     }
 
     public double getPos() {
-        return (rightWrist0.getPosition() + leftWrist2.getPosition()) / 2;
+        return rightWrist0.getPosition();
     }
 
     public void setPos(double position) {
         rightWrist0.setPosition(position);
-        leftWrist2.setPosition(position);
     }
 
     public void teleop(Gamepad gamepad2) {
-
+        teleopAngle = getAngle();
         if (gamepad2.a) {
-            teleopAngle = WristConstants.homeAngle;
-        } else if (gamepad2.x) {
             teleopAngle = WristConstants.scoreAngle;
+        } else if (gamepad2.x) {
+            teleopAngle = WristConstants.homeAngle;
+        } else if (gamepad2.b) {
+            teleopAngle = WristConstants.midAngle;
         } else {
             teleopAngle += gamepad2.right_trigger / 2 - gamepad2.left_trigger / 2;
         }
@@ -65,8 +55,6 @@ public class Wrist {
     public void periodic(Telemetry telemetry) {
         telemetry.addLine("Wrist:");
         telemetry.addLine("Wrist Angle: " + getAngle());
-        telemetry.addLine("Left Wrist Angle: " + getLeftAngle());
-        telemetry.addLine("Right Wrist Angle: " + getRightAngle());
         telemetry.addLine("Wrist Pos: " + getPos());
     }
 }
